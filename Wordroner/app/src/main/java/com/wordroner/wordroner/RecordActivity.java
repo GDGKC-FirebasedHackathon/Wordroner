b7cf227629d8a7bc763c09afdda8485f33a5374b
HeeIll <showx123@naver.com>
2017-02-16 14:47:02 +0900

    login with google plugin completed

--- Wordroner/app/src/main/java/com/wordroner/wordroner/RecordActivity.java
+++ Wordroner/app/src/main/java/com/wordroner/wordroner/RecordActivity.java
@@ -1,7 +1,10 @@
 package com.wordroner.wordroner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RecordActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class RecordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        final Button btn_record = (Button) findViewById(R.id.btn_record);
+
+        btn_record.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String words[] = {"123","123","a","the","cat","cat","cat","dog","apple","a"};
+                WordMap wordmap = new WordMap(words);
+
+                //
+                //add google speech API here
+                //
+
+                Intent intent = new Intent(getApplicationContext(), SelectActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
