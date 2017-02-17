2efa616a76f78226275315e46617a92ee31583c1
nojong <jake8955@naver,com>
2017-02-18 00:05:31 +0900

    add google speech

--- Wordroner/app/src/main/java/com/wordroner/wordroner/RecordActivity.java
+++ Wordroner/app/src/main/java/com/wordroner/wordroner/RecordActivity.java
@@ -1,59 +1,101 @@
 package com.wordroner.wordroner;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.wordroner.wordroner.recordManager.MyRecyclerAdapter;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 
 public class RecordActivity extends AppCompatActivity {
+
+    private TextView txtSpeechInput;
+    private ImageButton btnSpeak;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
 
-        final Button btn_record = (Button) findViewById(R.id.btn_record);
-
-        btn_record.setOnClickListener(new Button.OnClickListener(){
+        txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
+        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+        initLayout();
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                ArrayList<String> words = new ArrayList<String>(
-                        Arrays.asList("123", "123", "a", "the", "cat", "cat", "cat", "dog", "apple", "a", "starbucks")); //this is for test
-
-                //
-                //add google speech API here
-                //
-
-                WordMap wordmap = new WordMap(words);
-
-                String uid = "none";
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                if (user != null) {
-                    uid = user.getUid();
-                }
-
-                // Write a message to the database
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference(uid);
-
-
-                myRef.child("object").setValue(wordmap.ExtractWords());
-                //myRef.child("words").child("3").setValue("hello world2");
-
-
-                Intent intent = new Intent(getApplicationContext(), SelectActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                promptSpeechInput();
             }
         });
+    }
+    private void initLayout() {
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+    }
+    private void initData(ArrayList<String> result) {
+        List<String> speechList = result;
+        Log.d("어레이리스트 initData 확인", "initData: " + result);
+        recyclerView.setAdapter(new MyRecyclerAdapter(speechList));
+        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+    }
+    /**
+     * Showing google speech input dialog
+     */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en_US");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    /**
+     * Receiving speech input
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    Toast.makeText(this, String.valueOf(result.size()) + " : " + result.toString(), Toast.LENGTH_LONG).show();
+                    Log.d("어레이리스트확인", "onActivityResult: " + result.getClass());
+                    txtSpeechInput.setText(result.get(0));
+                    initData(result);
+                }
+                break;
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
